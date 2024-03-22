6044c9f7ac3bc89ad261d699ed20fc512139b7a0
Etheryen <lukasz.ciwoniuk@gmail.com>
2024-03-23 00:14:10 +0100

    fix(base-url): appearing as localhost in prod

--- src/utils/base-url.ts
+++ src/utils/base-url.ts
@@ -1,4 +1,7 @@
+import { env } from "~/env";
+
 export const getBaseUrl = () => {
+  if (env.NEXT_PUBLIC_PRODUCTION_URL) return env.NEXT_PUBLIC_PRODUCTION_URL;
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 };
